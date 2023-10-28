95ddff06d20733c4c00928548b05b2d916047170
update-rates.sh <update-rates@noreply.codeworth-gh.com>
2023-10-28 00:40:42 +0000

    update rates

--- docs/js/rates.js
+++ docs/js/rates.js
@@ -1,6 +1,6 @@
-const GSPC=4137.23;
+const GSPC=4117.37;
 const TA_125=1608.42;
-const USD_ILS=4.0738;
-const EUR_ILS=4.2911;
-const SAMPLE_TIME="Fri Oct 27 00:41:34 UTC 2023";
+const USD_ILS=4.0689;
+const EUR_ILS=4.3018;
+const SAMPLE_TIME="Sat Oct 28 00:40:42 UTC 2023";
 
