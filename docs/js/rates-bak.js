6ec7626649482b99b4639d451bfcf16f4f8abac7
update-rates.sh <update-rates@noreply.codeworth-gh.com>
2023-11-04 00:42:19 +0000

    update rates

--- docs/js/rates-bak.js
+++ docs/js/rates-bak.js
@@ -1,6 +1,6 @@
-const GSPC=4237.86;
-const TA_125=1669.63;
-const USD_ILS=3.9901;
-const EUR_ILS=4.2065;
-const SAMPLE_TIME="Thu Nov 2 00:42:33 UTC 2023";
+const GSPC=4317.78;
+const TA_125=1701.66;
+const USD_ILS=3.9783;
+const EUR_ILS=4.2243;
+const SAMPLE_TIME="Fri Nov 3 00:42:59 UTC 2023";
 
