d8de8d2285281bd29fa7e365e0c8334d86d3d066
update-rates.sh <update-rates@noreply.codeworth-gh.com>
2024-02-15 00:42:53 +0000

    update rates

--- docs/js/rates-bak.js
+++ docs/js/rates-bak.js
@@ -1,6 +1,6 @@
-const GSPC=5021.84;
-const TA_125=1875.56;
-const USD_ILS=3.6475;
-const EUR_ILS=3.9203;
-const SAMPLE_TIME="Tue Feb 13 00:42:53 UTC 2024";
+const GSPC=4953.17;
+const TA_125=1887.19;
+const USD_ILS=3.6611;
+const EUR_ILS=3.9123;
+const SAMPLE_TIME="Wed Feb 14 00:43:08 UTC 2024";
 
