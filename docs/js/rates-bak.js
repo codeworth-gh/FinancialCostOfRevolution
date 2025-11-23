965dbae4804d87117f3bf07e30fa7a75ab9057dc
update-rates.sh <update-rates@noreply.codeworth-gh.com>
2025-11-23 01:17:07 +0000

    update rates

--- docs/js/rates-bak.js
+++ docs/js/rates-bak.js
@@ -2,5 +2,5 @@ const GSPC=;
 const TA_125=;
 const USD_ILS=;
 const EUR_ILS=;
-const SAMPLE_TIME="Fri Nov 21 01:07:02 UTC 2025";
+const SAMPLE_TIME="Sat Nov 22 01:04:50 UTC 2025";
 
--- docs/js/rates.js
+++ docs/js/rates.js
@@ -2,5 +2,5 @@ const GSPC=;
 const TA_125=;
 const USD_ILS=;
 const EUR_ILS=;
-const SAMPLE_TIME="Sat Nov 22 01:04:50 UTC 2025";
+const SAMPLE_TIME="Sun Nov 23 01:17:07 UTC 2025";
 
