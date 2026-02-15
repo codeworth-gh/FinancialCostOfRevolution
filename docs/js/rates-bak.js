f53ec72236ab18f1bcfde598e669b24021bde00c
update-rates.sh <update-rates@noreply.codeworth-gh.com>
2026-02-15 01:46:42 +0000

    update rates

--- docs/js/rates-bak.js
+++ docs/js/rates-bak.js
@@ -2,5 +2,5 @@ const GSPC=;
 const TA_125=;
 const USD_ILS=;
 const EUR_ILS=;
-const SAMPLE_TIME="Fri Feb 13 01:46:40 UTC 2026";
+const SAMPLE_TIME="Sat Feb 14 01:26:16 UTC 2026";
 
--- docs/js/rates.js
+++ docs/js/rates.js
@@ -2,5 +2,5 @@ const GSPC=;
 const TA_125=;
 const USD_ILS=;
 const EUR_ILS=;
-const SAMPLE_TIME="Sat Feb 14 01:26:16 UTC 2026";
+const SAMPLE_TIME="Sun Feb 15 01:46:42 UTC 2026";
 
